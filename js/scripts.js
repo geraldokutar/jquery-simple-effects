afc65c0aa74687f9cb5766fe926ef564df099ba2
geraldokutar <geraldpilk@gmail.com>
2018-07-25 09:39:43 +0300

    Add qquery to show and hide image

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,9 @@ $(document).ready(function(){
     $("img").show();
   });
 });
+$(document).ready(function(){
+  $("p").click(function(){
+    $(".walrus-showing").show();
+    $(".walrus-hidden").hide();
+  })
+})
